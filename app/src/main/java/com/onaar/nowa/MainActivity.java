6b9952a6965fa876ac7f0b370ac16397124f16ce
NikodemPT <name>
2026-03-13 12:08:27 +0100

    czas

--- app/src/main/java/com/onaar/nowa/MainActivity.java
+++ app/src/main/java/com/onaar/nowa/MainActivity.java
@@ -1,6 +1,9 @@
 package com.onaar.nowa;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int ileSekund;
+
+    TextView textViewCzas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        textViewCzas = findViewById(R.id.textViewCzas);
+        Handler handler = new Handler();
+        handler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        ileSekund++;
+                        textViewCzas.setText(""+ileSekund);
+                        handler.postDelayed(this, 1000);
+                    }
+                }
+            );
+        }
     }
-}
